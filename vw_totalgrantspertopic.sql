9fe42d45927231d7d9712498d9bbda99f01e10dd
LauraMcCormack <laura.mccormack227@gmail.com>
2014-02-11 19:31:20 +0000

    new view for grants per topic
    added a new view to get the number of grants in each topic - TopicId and GrantCount (number of grants for that topic) stored

---
+++ vw_totalgrantspertopic.sql
@@ -0,0 +1,9 @@
+-- View for total amount of grants in each topic
+CREATE OR REPLACE VIEW vw_totalgrantspertopic as
+SELECT tg.TopicId, COUNT(tg.TopicId) as GrantCount
+FROM information i
+INNER JOIN topicmap_grants tg ON (i.GrantRefNumber = tg.GrantRefNumber)
+WHERE HoldingOrganisationName = 'Heriot-Watt University'
+AND tg.TopicId IN (SELECT TopicId FROM vw_hw_grants)
+GROUP BY tg.TopicId
+ORDER BY tg.TopicId;
