76aa7616ce23834731661047bf6c4136c6ad28b2
LauraMcCormack <laura.mccormack227@gmail.com>
2014-02-11 19:33:50 +0000

    create new view schools contribution per topic
    Get the percentage each school contributes to a topic - based on number of grants

---
+++ vw_schoolscontrpertopic.sql
@@ -0,0 +1,10 @@
+-- View to get the percentage each school contributes to a topic (by number of grants)
+CREATE OR REPLACE VIEW vw_schoolscontrpertopic as
+SELECT (COUNT(i.GrantRefNumber)/gpt.GrantCount*100), tg.TopicId, i.HoldingDepartmentName
+FROM information i
+INNER JOIN topicmap_grants tg ON (i.GrantRefNumber = tg.GrantRefNumber)
+INNER JOIN vw_totalgrantspertopic gpt ON (tg.TopicId = gpt.TopicId)
+WHERE HoldingOrganisationName = 'Heriot-Watt University'
+AND tg.TopicId IN (SELECT TopicId FROM vw_hw_grants)
+GROUP BY tg.TopicId, i.HoldingDepartmentName
+ORDER BY tg.TopicId;
