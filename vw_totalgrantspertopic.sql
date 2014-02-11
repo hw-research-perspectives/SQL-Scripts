4f77c6ff21bd5646a96db3347461bdedb90c0899
LauraMcCormack <laura.mccormack227@gmail.com>
2014-02-11 19:38:22 +0000

    Add Total Grant for topic to view
    Added Sum of the grant values for each topic on to vw_totalgrantspertopic

--- vw_totalgrantspertopic.sql
+++ vw_totalgrantspertopic.sql
@@ -1,6 +1,6 @@
 -- View for total amount of grants in each topic
 CREATE OR REPLACE VIEW vw_totalgrantspertopic as
-SELECT tg.TopicId, COUNT(tg.TopicId) as GrantCount
+SELECT tg.TopicId, COUNT(tg.TopicId) as GrantCount, SUM(i.TotalGrantValue) as TotalGrant
 FROM information i
 INNER JOIN topicmap_grants tg ON (i.GrantRefNumber = tg.GrantRefNumber)
 WHERE HoldingOrganisationName = 'Heriot-Watt University'
