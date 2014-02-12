da35f74253592f8c7ec9e3ead991a5f975ae44cb
LauraMcCormack <laura.mccormack227@gmail.com>
2014-02-12 21:41:50 +0000

    Add Dates to view
    Added dates so can do date manipulation easier on schools contribution per topic

--- vw_schoolscontrpertopic.sql
+++ vw_schoolscontrpertopic.sql
@@ -1,6 +1,6 @@
 -- View to get the percentage each school contributes to a topic (by number of grants)
 CREATE OR REPLACE VIEW vw_schoolscontrpertopic as
-SELECT (COUNT(i.GrantRefNumber)/gpt.GrantCount*100), tg.TopicId, i.HoldingDepartmentName
+SELECT (COUNT(i.GrantRefNumber)/gpt.GrantCount*100), tg.TopicId, i.HoldingDepartmentName, i.StartDate, i.EndDate
 FROM information i
 INNER JOIN topicmap_grants tg ON (i.GrantRefNumber = tg.GrantRefNumber)
 INNER JOIN vw_totalgrantspertopic gpt ON (tg.TopicId = gpt.TopicId)
