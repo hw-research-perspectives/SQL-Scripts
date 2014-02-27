652e6f53f1e5cacdecb98f4db8d8bf8c66985930
LauraMcCormack <laura.mccormack227@gmail.com>
2014-02-27 16:34:16 +0000

    HW grants view for RCUK
    View for list of heriot watt grants in RCUk database - set null Department Names to 'Other'

---
+++ RCUK/vw_hw_grants_100.sql
@@ -0,0 +1,9 @@
+-- view for grants from heriot watt
+CREATE OR REPLACE VIEW vw_hw_grants_100 as
+SELECT tg.TopicId, i.GrantTitle, CASE i.OrganisationDepartment WHEN '' THEN 'Other' ELSE i.OrganisationDepartment END as OrganisationDepartment, i.TotalGrantValue FROM information i 
+INNER JOIN topicmap_grants_100 tg ON (i.ID = tg.ID)
+INNER JOIN topics_100 t ON (tg.TopicId = t.TopicId)
+INNER JOIN vw_hw_topics vw ON (vw.TopicId = tg.TopicId)
+WHERE tg.TopicId IN (SELECT TopicId FROM vw_hw_topics)
+AND OrganisationName = 'Heriot-Watt University'
+ORDER BY vw.TotalMoneyXProportion desc;
