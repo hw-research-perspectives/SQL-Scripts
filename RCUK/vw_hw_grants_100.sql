4ec27f66148351e6c5a08275d45a72d8b0c0ebca
LauraMcCormack <laura.mccormack227@gmail.com>
2014-02-27 16:36:10 +0000

    Fix incorrect view name used
    Forgot to fix the view name for joining to vw_hw_topics_100

--- RCUK/vw_hw_grants_100.sql
+++ RCUK/vw_hw_grants_100.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE VIEW vw_hw_grants_100 as
 SELECT tg.TopicId, i.GrantTitle, CASE i.OrganisationDepartment WHEN '' THEN 'Other' ELSE i.OrganisationDepartment END as OrganisationDepartment, i.TotalGrantValue FROM information i 
 INNER JOIN topicmap_grants_100 tg ON (i.ID = tg.ID)
 INNER JOIN topics_100 t ON (tg.TopicId = t.TopicId)
-INNER JOIN vw_hw_topics vw ON (vw.TopicId = tg.TopicId)
-WHERE tg.TopicId IN (SELECT TopicId FROM vw_hw_topics)
+INNER JOIN vw_hw_topics_100 vw ON (vw.TopicId = tg.TopicId)
+WHERE tg.TopicId IN (SELECT TopicId FROM vw_hw_topics_100)
 AND OrganisationName = 'Heriot-Watt University'
 ORDER BY vw.TotalMoneyXProportion desc;
