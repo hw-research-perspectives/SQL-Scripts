34a1220f4f977117d9bfa0a70425b61010434f5a
Tom Townsend <tom.towny@gmail.com>
2014-03-01 12:58:33 +0000

    total spend per school per topic
    Created a query to get the total spend per school per topic for heriot watt grants

---
+++ RCUK/vw_hw_totalspendbyschool.sql
@@ -0,0 +1,15 @@
+-- view to get the total spend per topic for each school
+create or replace view vw_hw_totalspendbyschool as
+select t.TopicId,
+(case i.OrganisationDepartment when 'Sch of Life Sciences' then sum(i.TotalGrantValue) else 0 end) AS LifeSciences,
+(case i.OrganisationDepartment when 'Sch of Engineering and Physical Science' then sum(i.TotalGrantValue) else 0 end) AS EngineeringAndPhysical,
+(case i.OrganisationDepartment when 'Sch of the Built Environment' then sum(i.TotalGrantValue) else 0 end) AS BuiltEnvironment,
+(case i.OrganisationDepartment when 'Sch of Management and Languages' then sum(i.TotalGrantValue) else 0 end) AS ManagementAndLanguages,
+(case i.OrganisationDepartment when 'Institute Of Petroleum Engineering' then sum(i.TotalGrantValue) else 0 end) AS Petroleum,
+(case i.OrganisationDepartment when 'S of Mathematical and Computer Sciences' then sum(i.TotalGrantValue) else 0 end) AS Macs,
+(case i.OrganisationDepartment when 'Technology and Research Services' then sum(i.TotalGrantValue) else 0 end) AS TechRes,
+(case i.OrganisationDepartment when 'Sch of Textiles and Design' then sum(i.TotalGrantValue) else 0 end) AS Textiles,
+(case i.OrganisationDepartment when 'Other' then sum(i.TotalGrantValue) else 0 end) AS Other 
+from vw_hw_grants i 
+join topicmap_grants_100 t on(i.ID = t.ID) 
+group by t.TopicID,i.OrganisationDepartment;
