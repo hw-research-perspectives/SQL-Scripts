3050ed6f422956a4136d64523ec29c0ef4634d39
Tom Townsend <tom.towny@gmail.com>
2014-03-01 18:49:07 +0000

    wrong view used
    fix using 100 hw topic view to using 1000 hw topic view

--- RCUK/vw_hw_topicwords_1000.sql
+++ RCUK/vw_hw_topicwords_1000.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW vw_hw_topicwords_1000 AS
 SELECT TopicId, GROUP_CONCAT(TopicWord) as Words
 FROM topicwords_1000
-WHERE TopicId IN (SELECT TopicId FROM vw_hw_topics_100)
+WHERE TopicId IN (SELECT TopicId FROM vw_hw_topics_1000)
 GROUP BY TopicId;
