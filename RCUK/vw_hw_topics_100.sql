8f992f7fd971b2e09996930aa0449fbf069fc6c5
LauraMcCormack <laura.mccormack227@gmail.com>
2014-03-15 17:49:12 +0000

    update hw topic view to include start/end dates
    Update the view for 100 topic model for HW to include the start and end dates of the topics.

--- RCUK/vw_hw_topics_100.sql
+++ RCUK/vw_hw_topics_100.sql
@@ -1,9 +1,8 @@
 -- heriot watt specific topics for 100 topic model
 CREATE OR REPLACE VIEW vw_hw_topics_100 as 
-SELECT SUM(i.TotalGrantValue*tg.Proportion) as TotalMoneyXProportion, tg.TopicId, t.TopicLabel 
-FROM information i
+SELECT SUM(i.TotalGrantValue) as TotalMoney, tg.TopicId, t.TopicLabel, min(StartDate) as TopicStart, max(EndDate) as TopicEnd
+FROM vw_hw_grants i
 INNER JOIN topicmap_grants_100 tg ON (i.ID = tg.ID)
 INNER JOIN topics_100 t ON (tg.TopicId = t.TopicId)
-WHERE OrganisationName = 'Heriot-Watt University'
 GROUP BY tg.topicId
 ORDER BY 1 desc;
