9082ffee09768db2056476e0504455d73aae7bde
Simon Walker <github@stwalkerster.co.uk>
2014-04-08 12:33:06 +0100

    update view for SOM data + temp table

--- RCUK/vw_hw_topics_100.sql
+++ RCUK/vw_hw_topics_100.sql
@@ -1,8 +1,19 @@
 -- heriot watt specific topics for 100 topic model
 CREATE OR REPLACE VIEW vw_hw_topics_100 as 
-SELECT SUM(i.TotalGrantValue) as TotalMoney, tg.TopicId, t.TopicLabel, min(StartDate) as TopicStart, max(EndDate) as TopicEnd
+SELECT SUM(i.TotalGrantValue) as TotalMoney, tg.TopicId, t.TopicLabel, min(StartDate) as TopicStart, max(EndDate) as TopicEnd, i.OrganisationDepartment
 FROM vw_hw_grants i
 INNER JOIN topicmap_grants_100 tg ON (i.ID = tg.ID)
 INNER JOIN topics_100 t ON (tg.TopicId = t.TopicId)
 GROUP BY tg.topicId
 ORDER BY 1 desc;
+
+CREATE TABLE `tmp_vw_hw_topics_100` (
+  `TotalMoney` decimal(32,0) DEFAULT NULL,
+  `TopicId` int(11) NOT NULL,
+  `TopicLabel` longtext,
+  `TopicStart` datetime DEFAULT NULL,
+  `TopicEnd` datetime DEFAULT NULL,
+  `OrganisationDepartment` longtext,
+  `connection` int(11) NOT NULL,
+  PRIMARY KEY (`TopicId`,`connection`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
