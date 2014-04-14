456f290233fa86ae1c7aecbf0057fd84da71777b
kit1c2000 <tkl6@hw.ac.uk>
2014-04-14 04:53:16 +0100

    updated, added tables
    vw_hw_topics_100 view updated to show the school of highest total spend
    for each topic. added vw_hw_schoolofhighesttotalspend for getting the
    school of highest total spend for each topic. added
    vw_hw_totalspendbyschool_in_single_row for getting the
    totalspendbyschool for each topic in a single row.

--- RCUK/vw_hw_topics_100.sql
+++ RCUK/vw_hw_topics_100.sql
@@ -1,9 +1,10 @@
 -- heriot watt specific topics for 100 topic model
 CREATE OR REPLACE VIEW vw_hw_topics_100 as 
-SELECT SUM(i.TotalGrantValue) as TotalMoney, tg.TopicId, t.TopicLabel, min(StartDate) as TopicStart, max(EndDate) as TopicEnd, i.OrganisationDepartment
+SELECT SUM(i.TotalGrantValue) as TotalMoney, tg.TopicId, t.TopicLabel, min(StartDate) as TopicStart, max(EndDate) as TopicEnd, s.SchoolofHighestTotalSpend as OrganisationDepartment
 FROM vw_hw_grants i
 INNER JOIN topicmap_grants_100 tg ON (i.ID = tg.ID)
 INNER JOIN topics_100 t ON (tg.TopicId = t.TopicId)
+INNER JOIN vw_hw_schoolofhighesttotalspend s ON (`tg`.`TopicID` = `s`.`TopicID`)
 GROUP BY tg.topicId
 ORDER BY 1 desc;
 
--- RCUK/vw_hw_totalspendbyschool_in_single_row.sql
+++ RCUK/vw_hw_totalspendbyschool_in_single_row.sql
@@ -1,16 +1,16 @@
--- view to get the total spend per topic for each school
+-- view to get the total spend per topic for each school in a single row
 create or replace VIEW `vw_hw_totalspendbyschool_in_single_row` AS
 select 
 `vw_hw_totalspendbyschool`.`TopicId` AS `TopicID`,
-sum(`vw_hw_totalspendbyschool`.`LifeSciences`) AS `sum(LifeSciences)`,
-sum(`vw_hw_totalspendbyschool`.`EngineeringAndPhysical`) AS `sum(EngineeringAndPhysical)`,
-sum(`vw_hw_totalspendbyschool`.`BuiltEnvironment`) AS `sum(BuiltEnvironment)`,
-sum(`vw_hw_totalspendbyschool`.`ManagementAndLanguages`) AS `sum(ManagementAndLanguages)`,
-sum(`vw_hw_totalspendbyschool`.`Petroleum`) AS `sum(Petroleum)`,
-sum(`vw_hw_totalspendbyschool`.`Macs`) AS `sum(Macs)`,
-sum(`vw_hw_totalspendbyschool`.`TechRes`) AS `sum(TechRes)`,
-sum(`vw_hw_totalspendbyschool`.`Textiles`) AS `sum(Textiles)`,
-sum(`vw_hw_totalspendbyschool`.`Other`) AS `sum(Other)`
+sum(`vw_hw_totalspendbyschool`.`LifeSciences`) AS `LifeSciences`,
+sum(`vw_hw_totalspendbyschool`.`EngineeringAndPhysical`) AS `EngineeringAndPhysical`,
+sum(`vw_hw_totalspendbyschool`.`BuiltEnvironment`) AS `BuiltEnvironment`,
+sum(`vw_hw_totalspendbyschool`.`ManagementAndLanguages`) AS `ManagementAndLanguages`,
+sum(`vw_hw_totalspendbyschool`.`Petroleum`) AS `Petroleum`,
+sum(`vw_hw_totalspendbyschool`.`Macs`) AS `Macs`,
+sum(`vw_hw_totalspendbyschool`.`TechRes`) AS `TechRes`,
+sum(`vw_hw_totalspendbyschool`.`Textiles`) AS `Textiles`,
+sum(`vw_hw_totalspendbyschool`.`Other`) AS `Other`
 from
 `vw_hw_totalspendbyschool`
 group by `vw_hw_totalspendbyschool`.`TopicId`
