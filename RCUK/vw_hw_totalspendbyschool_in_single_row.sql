96efcb40c454b86a9d71b1b4af3c8412e62caf60
kit1c2000 <tkl6@hw.ac.uk>
2014-04-15 23:24:15 +0100

    Update vw_hw_totalspendbyschool_in_single_row.sql

--- RCUK/vw_hw_totalspendbyschool_in_single_row.sql
+++ RCUK/vw_hw_totalspendbyschool_in_single_row.sql
@@ -1,16 +1,14 @@
 -- view to get the total spend per topic for each school in a single row
-create or replace VIEW `vw_hw_totalspendbyschool_in_single_row` AS
-select 
-`vw_hw_totalspendbyschool`.`TopicId` AS `TopicID`,
-sum(`vw_hw_totalspendbyschool`.`LifeSciences`) AS `LifeSciences`,
-sum(`vw_hw_totalspendbyschool`.`EngineeringAndPhysical`) AS `EngineeringAndPhysical`,
-sum(`vw_hw_totalspendbyschool`.`BuiltEnvironment`) AS `BuiltEnvironment`,
-sum(`vw_hw_totalspendbyschool`.`ManagementAndLanguages`) AS `ManagementAndLanguages`,
-sum(`vw_hw_totalspendbyschool`.`Petroleum`) AS `Petroleum`,
-sum(`vw_hw_totalspendbyschool`.`Macs`) AS `Macs`,
-sum(`vw_hw_totalspendbyschool`.`TechRes`) AS `TechRes`,
-sum(`vw_hw_totalspendbyschool`.`Textiles`) AS `Textiles`,
-sum(`vw_hw_totalspendbyschool`.`Other`) AS `Other`
-from
-`vw_hw_totalspendbyschool`
-group by `vw_hw_totalspendbyschool`.`TopicId`
+create or replace VIEW vw_hw_totalspendbyschool_in_single_row AS
+select TopicID,
+sum(LifeSciences) AS LifeSciences,
+sum(EngineeringAndPhysical) AS EngineeringAndPhysical,
+sum(BuiltEnvironment) AS BuiltEnvironment,
+sum(ManagementAndLanguages) AS ManagementAndLanguages,
+sum(Petroleum) AS Petroleum,
+sum(Macs) AS Macs,
+sum(TechRes) AS TechRes,
+sum(Textiles) AS Textiles,
+sum(Other) AS Other
+from vw_hw_totalspendbyschool
+group by TopicID
