b6e6c76c5ead73ebabd12b69b973a9b5526d4025
LauraMcCormack <laura.mccormack227@gmail.com>
2014-04-16 20:44:24 +0100

    view to get the total spend for departments in topics
    View to be used for getting the department with the highest spend when choosing dept for colour coding word maps.

---
+++ RCUK/vw_topic_depts.sql
@@ -0,0 +1,5 @@
+-- view to get a list of topic departments with total spend for the topic
+CREATE OR REPLACE VIEW vw_topic_depts AS
+SELECT tg.TopicId, SUM(TotalGrantValue) as DeptGrant, i.OrganisationDepartment FROm vw_hw_grants i
+INNER JOIN topicmap_grants_100 tg on tg.ID = i.ID
+GROUP BY tg.TopicId, i.OrganisationDepartment;
