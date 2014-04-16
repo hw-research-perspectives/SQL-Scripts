95f30e4f39f6d83323a14933bf70961a0dd9fedb
LauraMcCormack <laura.mccormack227@gmail.com>
2014-04-16 21:46:10 +0100

    remove OrganisationDepartment from view
    removed the OrganisationDepartment from the hw topics 100 view as it is no longer used

--- RCUK/vw_hw_topics_100.sql
+++ RCUK/vw_hw_topics_100.sql
@@ -1,10 +1,9 @@
 -- heriot watt specific topics for 100 topic model
 CREATE OR REPLACE VIEW vw_hw_topics_100 as 
-SELECT SUM(i.TotalGrantValue) as TotalMoney, tg.TopicId, t.TopicLabel, min(StartDate) as TopicStart, max(EndDate) as TopicEnd, s.SchoolofHighestTotalSpend as OrganisationDepartment
+SELECT SUM(i.TotalGrantValue) as TotalMoney, tg.TopicId, t.TopicLabel, min(StartDate) as TopicStart, max(EndDate) as TopicEnd
 FROM vw_hw_grants i
 INNER JOIN topicmap_grants_100 tg ON (i.ID = tg.ID)
 INNER JOIN topics_100 t ON (tg.TopicId = t.TopicId)
-INNER JOIN vw_hw_schoolofhighesttotalspend s ON (`tg`.`TopicID` = `s`.`TopicID`)
 GROUP BY tg.topicId
 ORDER BY 1 desc;
 
@@ -14,7 +13,6 @@ CREATE TABLE `tmp_vw_hw_topics_100` (
   `TopicLabel` longtext,
   `TopicStart` datetime DEFAULT NULL,
   `TopicEnd` datetime DEFAULT NULL,
-  `OrganisationDepartment` longtext,
   `connection` int(11) NOT NULL,
   PRIMARY KEY (`TopicId`,`connection`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
