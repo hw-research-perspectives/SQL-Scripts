99ff79f4062d9242ca387a187c2bc158b2295f63
Simon Walker <github@stwalkerster.co.uk>
2014-04-17 10:50:36 +0100

    update

--- RCUK/vw_hw_grants.sql
+++ RCUK/vw_hw_grants.sql
@@ -1,4 +1,19 @@
 -- view for grants from heriot watt
-CREATE OR REPLACE VIEW vw_hw_grants as
-SELECT i.GrantTitle, i.GrantRefNumber, CASE i.OrganisationDepartment WHEN '' THEN 'Other' ELSE i.OrganisationDepartment END as OrganisationDepartment, i.TotalGrantValue FROM information i
-WHERE OrganisationName = 'Heriot-Watt University';
+CREATE OR REPLACE VIEW `meng_rcuk`.`vw_hw_grants` AS
+    select 
+        `i`.`ID` AS `ID`,
+        `i`.`GrantRefNumber` AS `GrantRefNumber`,
+        `i`.`GrantTitle` AS `GrantTitle`,
+        `i`.`GrantCategory` AS `GrantCategory`,
+        (case `i`.`OrganisationDepartment`
+            when '' then 'Other'
+            else `i`.`OrganisationDepartment`
+        end) AS `OrganisationDepartment`,
+        `i`.`TotalGrantValue` AS `TotalGrantValue`,
+        `i`.`StartDate` AS `StartDate`,
+        `i`.`EndDate` AS `EndDate`,
+        `i`.`Funder` AS `Funder`
+    from
+        `meng_rcuk`.`information` `i`
+    where
+        (`i`.`OrganisationName` = 'Heriot-Watt University');
--- RCUK/vw_hw_topics_100.sql
+++ RCUK/vw_hw_topics_100.sql
@@ -1,11 +1,6 @@
 -- heriot watt specific topics for 100 topic model
-CREATE OR REPLACE VIEW vw_hw_topics_100 as 
-SELECT SUM(i.TotalGrantValue) as TotalMoney, tg.TopicId, t.TopicLabel, min(StartDate) as TopicStart, max(EndDate) as TopicEnd
-FROM vw_hw_grants i
-INNER JOIN topicmap_grants_100 tg ON (i.ID = tg.ID)
-INNER JOIN topics_100 t ON (tg.TopicId = t.TopicId)
-GROUP BY tg.topicId
-ORDER BY 1 desc;
+CREATE OR REPLACE VIEW `meng_rcuk`.`vw_hw_topics_100` AS select sum((`i`.`TotalGrantValue` * `tg`.`Proportion`)) AS `TotalMoney`,`tg`.`TopicID` AS `TopicId`,`t`.`TopicLabel` AS `TopicLabel`,min(`i`.`StartDate`) AS `TopicStart`,max(`i`.`EndDate`) AS `TopicEnd` from ((`meng_rcuk`.`vw_hw_grants` `i` join `meng_rcuk`.`topicmap_grants_100` `tg` on((`i`.`ID` = `tg`.`ID`))) join `meng_rcuk`.`topics_100` `t` on((`tg`.`TopicID` = `t`.`TopicID`))) group by `tg`.`TopicID` order by 1 desc;
+
 
 CREATE TABLE `tmp_vw_hw_topics_100` (
   `TotalMoney` decimal(32,0) DEFAULT NULL,
